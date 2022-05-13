70e0f6f7c47dc13e7823d14a1ef042ac0eb09cde
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-05-14 00:00:54 +0530

    list and key

--- component-lifecycle/src/App.js
+++ component-lifecycle/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Button from "./Button";
-import Header from "./Lifecycle";
-import Clock from "./Clock";
-import Counter from "./Counter";
+import Button from "./components/Button";
+import Header from "./components/Lifecycle";
+import Clock from "./components/Clock";
+import Counter from "./components/Counter";
+import Show from "./components/Show";
 
-import Color from "./Color";
+import Color from "./components/Color";
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -23,10 +24,14 @@ export default class App extends Component {
       <div>
         <center>
           <Header favcol="yellow" />
+
+          <h3>Unmounting: componentWillUnmount</h3>
           {/* <Clock /> */}
           <Button />
+          <h3>Updating Phase:- shouldComponentUpdate</h3>
           <Color />
-
+          <Show />
+          <h3>Updating Phase:- componentDidUpdate</h3>
           <Counter number={this.state.counter} />
           <button onClick={this.increase}> Click Me</button>
         </center>
